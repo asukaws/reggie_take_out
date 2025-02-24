86e703ab5a6f7dda17b3e800efe02585bf6296b2
KOU <103808703+asukaws@users.noreply.github.com>
2025-02-24 18:11:27 +0900

    update test

--- src/main/java/com/itheima/reggie/ReggieApplication.java
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -11,6 +11,6 @@ public class ReggieApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReggieApplication.class, args);
-		log.info("start..");
+		log.info("start...");
 	}
 }
